b6737c6d036ae38a3f525691de1dda1342070f47
Houssam <Eng.houssam.sawan@gmail.com>
2025-10-28 10:56:17 +0000

    create bulk notification doctype

---
+++ whatsapp_notifier/whatsapp_notifier/doctype/whatsapp_bulk_notification/test_whatsapp_bulk_notification.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, eng.houssam.sawan@gmail.com and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestWhatsAppBulkNotification(FrappeTestCase):
+	pass
---
+++ whatsapp_notifier/whatsapp_notifier/doctype/whatsapp_bulk_notification/whatsapp_bulk_notification.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, eng.houssam.sawan@gmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('WhatsApp Bulk Notification', {
+	// refresh: function(frm) {
+
+	// }
+});
