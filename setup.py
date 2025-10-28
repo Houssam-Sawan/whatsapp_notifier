c77a022144f905b3648b7b9d387c0b2a2ae54658
Houssam <Eng.houssam.sawan@gmail.com>
2025-10-28 08:55:49 +0000

    Finish of one message implementation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 	install_requires = f.read().strip().split('\n')
 
 # get version from __version__ variable in posawesome/__init__.py
-from posawesome import __version__ as version
+from whatsapp_notifier import __version__ as version
 
 setup(
 	name='whatsapp_notifier',
--- whatsapp_notifier/app.py
+++ whatsapp_notifier/app.py
@@ -1,8 +1,8 @@
 import frappe
-from frappe_twilio_integration.api import send_whatsapp
+from twilio_integration.twilio_integration.doctype.whatsapp_message.whatsapp_message import WhatsAppMessage
 
 @frappe.whitelist()
-def send_whatsapp_message(to, message):
+def send_whatsapp(to, message):
     """
     Send WhatsApp message using Twilio Integration.
     Callable from Server Script or Client Script.
@@ -14,10 +14,24 @@ def send_whatsapp_message(to, message):
     number = ''.join([c for c in to if c.isdigit()])
     if not number.startswith("+"):
         number = f"+{number}"
+    numbers = [number]
 
+    doctype = None
+    docname = None
     try:
-        send_whatsapp(number, message)
+        WhatsAppMessage.send_whatsapp_message(numbers, message, doctype, docname)
         return f"Message sent successfully to {number}"
     except Exception as e:
-        frappe.log_error(f"WhatsApp Error: {e}", "WhatsApp Notifier")
+        #frappe.log_error(f"WhatsApp Error: {e}", "WhatsApp Notifier")
         frappe.throw(f"Failed to send message: {str(e)}")
+
+
+@frappe.whitelist()
+def test_method(name):
+    return f"hello {name}"
+
+@frappe.whitelist()
+def test_method_noargs():
+    return f"hello No args"
+
+
